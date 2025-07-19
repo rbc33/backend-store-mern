cb64420a85748c2fea0ea9526e9fdd40f0838d7d
rbc33 <ricardobenthem@gmail.com>
2025-07-19 11:29:36 +0200

    method get and handler

--- handlers/productHandler.js
+++ handlers/productHandler.js
@@ -1,3 +1,4 @@
+import product from '../models/product.js'
 import Product from '../models/product.js'
 
 export const addProduct = async (req, res) => {
@@ -20,4 +21,9 @@ export const addProduct = async (req, res) => {
 	} catch (e) {
 		res.status(500).send({ message: e.message })
 	}
+}
+// TODO: Add pagination
+export const getProducts = async (rew, res) => {
+	const products = await Product.find().lean().exec()
+	res.status(200).send(products)
 }
--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { addProduct } from '../handlers/productHandler.js'
+import { addProduct, getProducts } from '../handlers/productHandler.js'
 import upload from '../libs/storage.js'
 
 const api = express.Router()
 
 api.post('/products', upload.single('image'), addProduct)
+api.get('/products', getProducts)
 
 export default api
